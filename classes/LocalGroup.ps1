33bb07c1a0f0781db731c3afa0b5193e3f8a558b
Brian Hewitt <durendal@skynet.lan>
2020-10-14 23:14:24 -0600

    added common hidden Init method for constructors to call

--- classes/LocalGroup.ps1
+++ classes/LocalGroup.ps1
@@ -3,46 +3,54 @@ Start-Sleep -Milliseconds 1
 $Path = $MyInvocation.MyCommand.Path -split "\\"
 $Path = $Path[0..($Path.Length-3)] -join "\"
 
-class Group {
-  [object] $SID
-
-  Group([string] $Identifier, [string] $Description="") {
-    try {
+class LocalGroup {
+  [object] $_SID
+  hidden [void] Init([string] $Identifier, [string] $Description) {
+    if($Description.Length > 0) {
+      New-LocalGroup -Name $Identifier -ErrorAction Stop
+      $this._SID = $(Get-LocalGroup -Name $Identifier).SID
+    } else {
       if($(Select-String -Pattern 'S-\d-(?:\d+-){1,14}\d+' -InputObject $Identifier).Matches) {
-        $this.SID = $(Get-LocalGroup -SID $Identifier -ErrorAction Stop).SID
+        Write-Host $(Get-LocalGroup -SID $Identifier).SID
+        $this._SID = $(Get-LocalGroup -SID $Identifier).SID
       } else {
-        $this.SID = $(Get-LocalGroup -Name $Identifier -ErrorAction Stop).SID
-      }
-    }
-    catch
-    {
-      if($(Select-String -Pattern 'S-\d-(?:\d+-){1,14}\d+' -InputObject $Identifier).Matches) {
-        throw "Invalid group name"
-      } else {
-        New-LocalGroup -Name $Identifier -Description $Description -ErrorAction Stop
-        $this.SID = $(Get-LocalGroup -Name $Identifier -ErrorAction Stop).SID
+        Write-Host $(Get-LocalGroup -Name $Identifier).SID
+        $this._SID = $(Get-LocalGroup -Name $Identifier).SID
       }
     }
   }
 
-  [string] GetSID() {
-    return $this.SID.Value
+  LocalGroup([string] $Identifier) {
+    Write-Host "Identifier: $Identifier"
+    $this.Init($Identifier, "")
+  }
+
+  LocalGroup([string] $Identifier, [string] $Description) {
+    Write-Host "Identifier: $Identifier"
+    Write-Host "Description: $Description"
+    $this.Init($Identifier, $Description)
+  }
+
+  [object] GetSID() {
+    return $this._SID
   }
 
   [string] GetGroupName() {
-    return Get-LocalGroup -SID $this.GetSID() | select -ExpandProperty Name
+    $s = $this.GetSID()
+    Write-Output "SID: $s"
+    return $(Get-LocalGroup -SID $this.GetSID() | select -ExpandProperty Name)
   }
 
   [string] GetDescription() {
-    return Get-LocalGroup -Name $this.GetGroupName() | select -ExpandProperty Description
+    return $(Get-LocalGroup -Name $this.GetGroupName() | select -ExpandProperty Description)
   }
 
   [string] GetObjectClass() {
-    return Get-LocalGroup -Name $this.GetGroupName() | select -ExpandProperty ObjectClass
+    return $(Get-LocalGroup -Name $this.GetGroupName() | select -ExpandProperty ObjectClass)
   }
 
   [string] GetPrincipalSource() {
-    return Get-LocalGroup -Name $this.GetGroupName() | select -ExpandProperty PrincipalSource
+    return $(Get-LocalGroup -Name $this.GetGroupName() | select -ExpandProperty PrincipalSource)
   }
 
 }
